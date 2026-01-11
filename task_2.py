27ad5126ed87f16ff8695a967d6ef0a61b4a9348
sergii-osaulenko <sergii.osaulenko@gmail.com>
2026-01-11 16:58:52 +0200

    Add Task 2

---
+++ task_2.py
@@ -0,0 +1,33 @@
+import turtle
+
+def draw_pithagoras_tree(t, branch_len, level):
+    if level == 0:
+        return
+    
+    t.forward(branch_len)
+    
+    angle = 45
+    t.left(angle)
+    draw_pithagoras_tree(t, branch_len * 0.7, level - 1)
+    
+    t.right(2 * angle)
+    draw_pithagoras_tree(t, branch_len * 0.7, level - 1)
+    
+    t.left(angle)
+    t.backward(branch_len)
+
+def main():
+    level = int(input("Введіть рівень рекурсії (наприклад, 7): "))
+    
+    t = turtle.Turtle()
+    t.speed("fastest")
+    t.left(90)
+    t.up()
+    t.goto(0, -200)
+    t.down()
+    
+    draw_pithagoras_tree(t, 100, level)
+    turtle.done()
+
+if __name__ == "__main__":
+    main()
